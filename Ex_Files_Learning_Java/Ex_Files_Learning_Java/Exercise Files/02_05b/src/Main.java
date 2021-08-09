2882f321640780e61ed4b065395b7824b56a838b
Force <thanawat.cu@gmail.com>
2021-08-09 23:01:01 +0700

    delete unnessary file

--- Ex_Files_Learning_Java/Ex_Files_Learning_Java/Exercise Files/02_05b/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-
-    public static void main(String[] args) {
-        double studentGPA = 3.45;
-        String studentFirstName = "Kayla";
-        String studentLastName = "Hammon";
-    }
-}
